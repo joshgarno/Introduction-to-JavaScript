bd50ee4ca97d7fd0cf63302248d7e33cf33c7ad6
Josh Garno <josgarno@gmail.com>
2020-08-25 18:50:26 -0400

    Task 6 done

--- index.js
+++ index.js
@@ -101,6 +101,21 @@ console.log(num1 + " feet/foot is equal to " + ftToCm(num1) + " centimeters");
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
 
+function annoyingSong(a) {
+  for (a = 99; a > 0; a--) {
+    console.log(
+      a +
+        " bottles of beer on the wall,  " +
+        a +
+        " bottles of beer, take one down pass it around " +
+        (a - 1) +
+        " bottles of beer on the wall"
+    );
+  }
+}
+
+console.log(annoyingSong());
+
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
 //write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade
